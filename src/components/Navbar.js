d8cb8e5f98d8aa3267c96c1c2be9301be5671eea
Thidaayewin <thidaayewin1@gmail.com>
2024-01-15 22:54:24 +0800

    AIPS-001/Added Sidebar into UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,51 @@
 import { useState } from "react"
+import Sidebar from "./Sidebar"
+import{faHome, faList, faCog} from "@fortawesome/free-solid-svg-icons"
+
 export default function Navbar(){
-    const [showSidebar,setShowSidebar]=useState(false)
+    const [showSidebar,setShowSidebar]=useState(false);
+    const links = [
+        {
+            name: "Home",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon: faList
+        },
+        {
+            name: "Settings",
+            path: "/settings",
+            icon: faCog
+        },
+
+    ]
+    function closeSidebar(){
+        setShowSidebar(false);
+    }
+
     return (
-        <div className="navbar container">
+        <>
+            <div className="navbar container">
             <a href="#!" className="logo">F<span>oo</span>diesHub</a>
             <div className="nav-links">
-            <a href="#!" className="active">Home</a>
+                {links.map(link=>(
+                    <a href="#!" key={link.name}>{link.name}</a>
+                ))}
+            {/* <a href="#!" className="active">Home</a>
             <a href="#!">Recipes</a>
-            <a href="#!">Settings</a>
+            <a href="#!">Settings</a> */}
             </div>
-            <div onClick={()=>setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+            <div onClick={()=>setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
         </div>
+        {showSidebar &&   <Sidebar close={closeSidebar} links={links}/>}
+      
+        </>
     )
 }
